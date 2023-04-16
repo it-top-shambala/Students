9b6993035e8d59915eb1992bc3d1efc7d2ec4bbf
Dekard89 <nasenkov_89@mail.ru>
2023-04-16 14:08:54 +0300

    добавил заголовочный файл и функцию по считыванию с файла( 2 вариант)

--- FileHelper.h
+++ FileHelper.h
@@ -45,3 +45,35 @@ Students ImportFromFile1(string path){
     file.close();
     return result;
 }
+string Parser(string& str,char findedChar){
+    string result=str.substr(0,str.find(findedChar));
+    str=str.substr(str.find(findedChar)+1);
+    return result;
+}
+Students ImportFromFile2(string path,string Parser(string& str,char findedChar)) {
+    Students result;
+    ifstream file;
+    file.open(path);
+    if (!file.is_open())
+        cerr << "Error" << endl;
+    Subjects subjects;
+    Mark mark;
+    Marks marks;
+    string name;
+    string subject;
+    string line;
+    while (getline(file,line)){
+        name= Parser(line,'-');
+        subject= Parser(line,':');
+        while (line.empty()){
+            mark= Parser(line,' ');
+            marks.push_back(mark);
+        }
+        marks.push_back(line);
+        subjects.insert(pair<string,Marks>(subject,marks));
+    }
+    result.insert(pair<string,Subjects>(name,subjects));
+    file.close();
+    return result;
+}
+void ExportToFile()
