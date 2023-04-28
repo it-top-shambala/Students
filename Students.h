3b64c2172f6f689b079fb8e307ccfb9826f173f0
CyborgMike <sel-pto@mail.ru>
2023-04-28 21:03:24 +0800

    Добавлены функции

--- Students.h
+++ Students.h
@@ -1,19 +1,11 @@
-/*
- * Студент 1:
- *      Предмет 1: 5,5,4,1
- *      Предмет 2: 5,5,4,1
- *      Предмет 3: 5,5,4,1
-* Студент 2:
- *      Предмет 1: 5,5,4,1
- *      Предмет 6: 5,5,4,1
- *      Предмет 3: 5,5,4,1
- */
-
 #pragma once
 
 #include <string>
 #include <vector>
 #include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -32,15 +24,81 @@ void InsertStudent(string name) {
     Subjects temp;
     _students.insert(Student(name, temp));
 }
+
+void RemoveStudent(string name) {
+    _students.erase(name);
+}
+
 void InsertSubject(string student_name, string subject_name) {
     Marks temp;
     _students[student_name].insert(Subject(subject_name, temp));
 }
+
+void RemoveSubject(string student_name, string subject_name) {
+    _students[student_name].erase(subject_name);
+}
+
 void InsertMark(string student_name, string subject_name, Mark mark) {
     _students[student_name][subject_name].push_back(mark);
 }
+
+void RemoveMark(string student_name, string subject_name, Mark mark) {
+    auto& marks = _students[student_name][subject_name];
+    marks.erase(remove(marks.begin(), marks.end(), mark), marks.end());
+}
+
 void InsertMarks(string student_name, string subject_name, Marks marks) {
     for (Mark mark : marks) {
         _students[student_name][subject_name].push_back(mark);
     }
+}
+
+void UpdateMark(string student_name, string subject_name, Mark old_mark, Mark new_mark) {
+    auto& marks = _students[student_name][subject_name];
+    replace(marks.begin(), marks.end(), old_mark, new_mark);
+}
+
+void ExportData(string filename) {
+    ofstream outfile(filename);
+    for (auto& student : _students) {
+        outfile << student.first << endl;
+        for (auto& subject : student.second) {
+            outfile << subject.first << endl;
+            for (auto& mark : subject.second) {
+                outfile << mark << ",";
+            }
+            outfile << endl;
+        }
+        outfile << endl;
+    }
+    outfile.close();
+    cout << "Данные экспортированы в файл " << filename << endl;
+}
+
+void ImportData(string filename) {
+    ifstream infile(filename);
+    string line;
+    string student_name;
+    string subject_name;
+    Mark mark;
+
+    while (getline(infile, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        if (line.back() == ':') {
+            student_name = line.substr(0, line.size() - 1);
+            InsertStudent(student_name);
+        } else if (isalpha(line[0])) {
+            subject_name = line;
+            InsertSubject(student_name, subject_name);
+        } else {
+            stringstream ss(line);
+            while (getline(ss, mark, ',')) {
+                InsertMark(student_name, subject_name, mark);
+            }
+        }
+    }
+    infile.close();
+    cout << "Данные импортированы из файла " << filename << endl;
 }
--- main.cpp
+++ main.cpp
@@ -26,5 +26,84 @@ int main() {
 
     PrintStudents();
 
-    return 0;
-}
+    int choice;
+    string name, subject, mark, old_mark, new_mark, filename;
+
+    while (true) {
+        ShowMenu();
+        cout << "Укажите пункт меню: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Введите имя студента: ";
+                cin >> name;
+                InsertStudent(name);
+                break;
+            case 2:
+                cout << "Введите имя студента: ";
+                cin >> name;
+                cout << "Введите название предмета: ";
+                cin >> subject;
+                InsertSubject(name, subject);
+                break;
+            case 3:
+                cout << "Введите имя студента: ";
+                cin >> name;
+                cout << "Введите название предмета: ";
+                cin >> subject;
+                cout << "Введите оценку: ";
+                cin >> mark;
+                InsertMark(name, subject, mark);
+                break;
+            case 4:
+                cout << "Введите имя студента: ";
+                cin >> name;
+                RemoveStudent(name);
+                break;
+            case 5:
+                cout << "Введите имя студента: ";
+                cin >> name;
+                cout << "Введите название предмета: ";
+                cin >> subject;
+                RemoveSubject(name, subject);
+                break;
+            case 6:
+                cout << "Введите имя студента: ";
+                cin >> name;
+                cout << "Введите название предмета: ";
+                cin >> subject;
+                cout << "Введите удаляемую оценку: ";
+                cin >> mark;
+                RemoveMark(name, subject, mark);
+                break;
+            case 7:
+                cout << "Введите имя студента: ";
+                cin >> name;
+                cout << "Введите название предмета: ";
+                cin >> subject;
+                cout << "Введите старую оценку: ";
+                cin >> old_mark;
+                cout << "Введите новую оценку: ";
+                cin >> new_mark;
+                UpdateMark(name, subject, old_mark, new_mark);
+                break;
+            case 8:
+                cout << "Введите название файла для экспорта данный: ";
+                cin >> filename;
+                ExportData(filename);
+                break;
+            case 9:
+                cout << "Введите название файла для экспорта данный: ";
+                cin >> filename;
+                ImportData(filename);
+                break;
+            case 10:
+                cout << "Выход";
+                break;
+
+        }
+    }
+
+        return 0;
+    }
