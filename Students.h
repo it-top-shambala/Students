f4169624f122907fdb62d03c479de21c34dc48d1
Алексей Шадура <129580672+alexSh14@users.noreply.github.com>
2023-05-05 11:25:59 +0700

    Add files via upload

--- Students.h
+++ Students.h
@@ -1,23 +1,10 @@
-/*
- * Студент 1:
- *      Предмет 1: 5,5,4,1
- *      Предмет 2: 5,5,4,1
- *      Предмет 3: 5,5,4,1
-* Студент 2:
- *      Предмет 1: 5,5,4,1
- *      Предмет 6: 5,5,4,1
- *      Предмет 3: 5,5,4,1
- */
-
 #pragma once
 
 #include <string>
 #include <vector>
 #include <map>
 
-using namespace std;
-
-using Mark = string;
+using Mark = string;       
 using Marks = vector<Mark>;
 
 using Subjects = map<string, Marks>;
@@ -26,8 +13,8 @@ using Subject = pair<string, Marks>;
 using Students = map<string, Subjects>;
 using Student = pair<string, Subjects>;
 
-Students _students;
-
+Students _students;       
+                         //A set of functions for adding information about student grades to the _students variable:                         
 void InsertStudent(string name) {
     Subjects temp;
     _students.insert(Student(name, temp));
@@ -35,12 +22,40 @@ void InsertStudent(string name) {
 void InsertSubject(string student_name, string subject_name) {
     Marks temp;
     _students[student_name].insert(Subject(subject_name, temp));
-}
+}                     
 void InsertMark(string student_name, string subject_name, Mark mark) {
     _students[student_name][subject_name].push_back(mark);
 }
 void InsertMarks(string student_name, string subject_name, Marks marks) {
     for (Mark mark : marks) {
         _students[student_name][subject_name].push_back(mark);
+    }
+}
+                     //A set of functions that are used to display information about students' grades on the screen.                     
+void PrintMark(Mark mark) {
+    cout << mark << ", ";
+}                 
+void PrintMarks(Marks marks) {
+    for (Mark mark : marks) {
+        PrintMark(mark);
+    }
+    cout << endl;
+}                    
+void PrintSubject(Subject subject) {
+    cout << subject.first << ": ";
+    PrintMarks(subject.second);
+}                    
+void PrintSubjects(Subjects subjects) {
+    for (Subject subject : subjects) {
+        PrintSubject(subject);
+    }
+}                   
+void PrintStudent(Student student) {
+    cout << student.first << ":" << endl;
+    PrintSubjects(student.second);
+}                    
+void PrintStudents() {
+    for (Student student : _students) {
+        PrintStudent(student);
     }
 }
